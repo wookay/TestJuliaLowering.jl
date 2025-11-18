7d793ce5e250946564515c1f684e60a26ec7f4f6
WooKyoung Noh <wookay.noh@gmail.com>
2025-11-19 08:46:58 +0900

    init

---
+++ src/TestJuliaLowering.jl
@@ -0,0 +1,17 @@
+module TestJuliaLowering
+
+"""
+    hello(who::String)
+
+Return "Hello, `who`".
+"""
+hello(who::String) = "Hello, $who"
+
+"""
+    domath(x::Number)
+
+Return `x + 5`.
+"""
+domath(x::Number) = x + 5
+
+end # module TestJuliaLowering
---
+++ test/julialowering/syntaxtree.jl
@@ -0,0 +1,25 @@
+using Jive
+@If VERSION >= v"1.13.0-DEV.880" module test_julialowering
+
+using Test
+using JuliaLowering: SyntaxTree, parsestmt
+using JuliaSyntaxFormatter: JuliaSyntaxFormatter, Text
+
+# from julia/JuliaLowering/test/demo.jl
+
+function formatsrc(ex; kws...)
+    Text(JuliaSyntaxFormatter.formatsrc(ex; kws...))
+end
+
+src = """
+module Foo
+end # module Foo
+"""
+ex = parsestmt(SyntaxTree, src, filename="foo.jl")
+@test ex._id == 1
+@test formatsrc(ex).content == """
+module Foo
+end\
+"""
+
+end # module test_julialowering
