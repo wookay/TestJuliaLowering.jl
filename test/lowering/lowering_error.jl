4134339980513dbfce13644fa0206be17ee12209
WooKyoung Noh <wookay.noh@gmail.com>
2026-01-08 20:53:40 +0900

    FAFO

--- test/lowering/lowering_error.jl
+++ test/lowering/lowering_error.jl
@@ -3,7 +3,15 @@ module test_julialowering_lowering_error
 using Test
 
 # code from julia/test/syntax.jl
-macro test_loweringerror(ex, msg, opt=nothing)
+macro test_loweringerror1(ex, msg, opt=nothing)
+    code = :(@test Meta.lower(@__MODULE__, $(esc(ex))) == Expr(:error, $msg))
+    code.args[2] = __source__
+    if opt === :broken
+        code.args[1] = :var"@test_broken"
+    end
+end
+
+macro test_loweringerror2(ex, msg, opt=nothing)
     code = :(@test Meta.lower(@__MODULE__, $(esc(ex))) == Expr(:error, $msg))
     code.args[2] = __source__
     if opt === :broken
@@ -12,6 +20,7 @@ macro test_loweringerror(ex, msg, opt=nothing)
     code
 end
 
-@test_loweringerror(Meta.parse("x..."), "\"...\" expression outside call")
+@test @macroexpand(@test_loweringerror1(Meta.parse("x..."), "FAFO")) === nothing
+@test @macroexpand(@test_loweringerror2(Meta.parse("x..."), "FAFO")) isa Expr
 
 end # module test_julialowering_lowering_error
