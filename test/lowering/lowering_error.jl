622c310d88913b28661e93f371a35fedb97d6661
WooKyoung Noh <wookay.noh@gmail.com>
2026-01-13 09:21:59 +0900

    `Expr(:error, esc(msg))` for test_loweringerror2

--- test/lowering/lowering_error.jl
+++ test/lowering/lowering_error.jl
@@ -12,7 +12,7 @@ macro test_loweringerror1(ex, msg, opt=nothing)
 end
 
 macro test_loweringerror2(ex, msg, opt=nothing)
-    code = :(@test Meta.lower(@__MODULE__, $(esc(ex))) == Expr(:error, $msg))
+    code = :(@test Meta.lower(@__MODULE__, $(esc(ex))) == Expr(:error, $(esc(msg))))
     code.args[2] = __source__
     if opt === :broken
         code.args[1] = :var"@test_broken"
