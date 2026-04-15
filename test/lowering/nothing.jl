db7c46a0f66104b27a5db351082743bde7a29e5e
WooKyoung Noh <wookay.noh@gmail.com>
2026-04-15 21:13:31 +0900

    K"nothing"

---
+++ test/lowering/nothing.jl
@@ -0,0 +1,12 @@
+module test_julialowering_lowering_nothing
+
+using Test
+using JuliaLowering: JuliaLowering as JL
+using JuliaSyntax: JuliaSyntax as JS
+using .JL: @K_str
+
+if VERSION >= v"1.14.0-DEV.2040"
+    @test K"nothing" isa JS.Kind
+end
+
+end # module test_julialowering_lowering_nothing
