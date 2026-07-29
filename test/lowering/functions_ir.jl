920d1b732a653b918685816da2d53fcd57420d7f
WooKyoung Noh <wookay.noh@gmail.com>
2026-07-29 17:31:30 +0900

    `Core.define_method`

---
+++ test/lowering/functions_ir.jl
@@ -0,0 +1,9 @@
+module test_lowering_functions_ir
+
+# from julia/JuliaLowering/test/functions_ir.jl
+
+if VERSION >= v"1.14.0-DEV.2828" # julia commit 8dc3ef6ac0
+Core.define_method
+end
+
+end # module test_lowering_functions_ir
